79b6410a33915aa0b230acef5afd5d496825a1fb
Afara <afarasidheek@gmail.com>
2022-07-04 17:37:37 +0400

    get a user

--- routes/user.js
+++ routes/user.js
@@ -1,58 +1,57 @@
-const router = require("express").Router()
-const { findByIdAndUpdate } = require("../models/User")
-const User = require("../models/User")
-const bcrypt = require("bcrypt")
-router.put("/:id", async (req , res) => {
-    if(req.body.userId === req.params.id|| req.body.isAdmin)
-    {
-       console.log(req.params.id)
-        if(req.body.password)
-        {
-            try{
-                const salt = await bcrypt.genSalt(10)
-                req.body.password = await bcrypt.hash(req.body.password,salt)
-            }
-            catch(err){
-                return res.status(500).json(err)
-            }
-        }
-        try {
-            const user = await  User.findByIdAndUpdate(req.params.id,{
-                $set : req.body
-            })
-            res.status(200).json("account has been updated")
-
-        }
-        catch(err){
-            return res.status(500).json(err)
-        }
+const router = require("express").Router();
+const { findByIdAndUpdate } = require("../models/User");
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    console.log(req.params.id);
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch (err) {
+        return res.status(500).json(err);
+      }
     }
-    else {
-        return res.status(403).json("you can update only your account")
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json("account has been updated");
+    } catch (err) {
+      return res.status(500).json(err);
     }
-})
+  } else {
+    return res.status(403).json("you can update only your account");
+  }
+});
 //get user
+
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 //delete user
 
-router.delete("/:id", async (req , res) => {
-    console.log("hai")
-    if(req.body.userId === req.params.id || req.body.isAdmin)
-    {
-
-        try {
-            const user = await  User.findByIdAndDelete(req.params.id)
-            res.status(200).json("account has been deleted")
-
-        }
-        catch(err){
-            return res.status(500).json(err)
-        }
+router.delete("/:id", async (req, res) => {
+  console.log("hai");
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("account has been deleted");
+    } catch (err) {
+      return res.status(500).json(err);
     }
-    else {
-        return res.status(403).json("you can delete only your account")
-    }
-})
+  } else {
+    return res.status(403).json("you can delete only your account");
+  }
+});
 //follow user
 //unfollow user
 
-module.exports = router
+module.exports = router;
